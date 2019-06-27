af7dc327f8a1b15aeb94df4e55e83771af20d169
Henrique Saenger <41701799+henriquesaenger@users.noreply.github.com>
2019-06-27 12:15:43 -0300

    Update person.hs

--- person.hs
+++ person.hs
@@ -1,7 +1,13 @@
 
+import System.Random
 
-data Player = Player {Position::Point, HP::Int, Energy::Int}
+data Player = Player {position::Point, hp::Int, energy::Int, attack::Int}
  deriving(Show)
 
-data Monster= Monster {Position::Point, HP::Int, Energy::Int, Attack::Int}
- deriving(Show)
+random:: ()->Int
+random ()= RandomR(1,20::Int)			--Devolve um número random entre 1 e 20(simulando a rolagem de um dado)
+
+
+ataque::Player-> Player-> Player 		--Recebem um jogador e um monstro e devolve um jogador
+ataque jogador(_ hp _ _) monstro(_ _ _ attack)=	if (random () <= 10) then () else (hp=hp-attack)		--Se o jogador tirar 10 ou menos não acontece nada, se tirar mais que 10, o ataque acontece
+
