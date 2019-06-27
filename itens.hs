f0f10d016bb611b8602e43119e3e113073bd1bfc
Henrique Saenger <41701799+henriquesaenger@users.noreply.github.com>
2019-06-27 15:49:19 -0300

    Update itens.hs

--- itens.hs
+++ itens.hs
@@ -1,5 +1,10 @@
 
 
-data Weapon= Weapon{nome::[Char],dano :: Int}
+data Weapon= Weapon{nome::[Char],dano :: int}
 
 data Cura= Cura{nome::[Char], heal::Int}
+
+data Item= Weapon | Cura
+
+data Mochila = [Item]
+
