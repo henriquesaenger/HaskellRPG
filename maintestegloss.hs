c560a9712c03f4fd5c385a0168769fd619c1c2d1
Gustavo Barbosa <gbdsantos@inf.ufpel.edu.br>
2019-06-28 00:22:41 -0300

    imagens

--- maintestegloss.hs
+++ maintestegloss.hs
@@ -18,22 +18,26 @@ main = play
  where
 
   windowDisplay :: Display
-  windowDisplay = InWindow "Window" (1024, 600) (10, 10)
+  windowDisplay = InWindow "Jonas saving the world!" (1024, 600) (10, 10)
 
   simulationRate :: Int
   simulationRate = 60
 
   initialModel :: World
-  initialModel = (0,0)
+  initialModel = (-490,-280)
 
   drawingFunc :: World -> Picture
-  drawingFunc (x, y) = translate x y pikaimg
+  drawingFunc (x, y) = pictures [translate x y pikaimg, translate x (y+50) pikaimg]
 
   inputHandler :: Event -> World -> World
-  inputHandler (EventKey (SpecialKey KeyUp) Down _ _) (x, y) = (x, y + 10)
-  inputHandler (EventKey (SpecialKey KeyDown) Down _ _) (x, y) = (x, y - 10)
-  inputHandler (EventKey (SpecialKey KeyRight) Down _ _) (x, y) = (x + 10, y)
-  inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) (x, y) = (x - 10, y)
+  inputHandler (EventKey (SpecialKey KeyUp) Down _ _) (x, y) = if y<(280) then (x, y + 10) else (x,y)
+  inputHandler (EventKey (SpecialKey KeyDown) Down _ _) (x, y) = if y>(-280) then (x, y - 10) else (x,y)
+  inputHandler (EventKey (SpecialKey KeyRight) Down _ _) (x, y) = if x<(490) then (x + 10, y) else (x,y)
+  inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) (x, y) = if x>(-490) then (x - 10, y) else (x,y)
+  inputHandler (EventKey (Char 'w') Down _ _) (x, y) = if y<(280) then (x, y + 10) else (x,y)
+  inputHandler (EventKey (Char 's') Down _ _) (x, y) = if y>(-280) then (x, y - 10) else (x,y)
+  inputHandler (EventKey (Char 'd') Down _ _) (x, y) = if x<(490) then (x + 10, y) else (x,y)
+  inputHandler (EventKey (Char 'a') Down _ _) (x, y) = if x>(-490) then (x - 10, y) else (x,y)
   inputHandler _ w = w
 
   updateFunc :: Float -> World -> World
