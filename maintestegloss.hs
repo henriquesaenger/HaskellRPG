b1f617c711b74300e64deab8574f80c2c509f601
Gustavo Barbosa <gbdsantos@inf.ufpel.edu.br>
2019-07-05 14:50:21 -0300

    mais modelos

--- maintestegloss.hs
+++ maintestegloss.hs
@@ -7,6 +7,12 @@ data World = Game
  , sbp::(Float, Float)--posicao sbp
  , matamoscas::(Float, Float)
  , matamoscasstate::(Float)--para alternar imagem o mata moscas
+ , raid::(Float, Float)--
+ , aranha::(Float, Float)--
+ , bomba::(Float, Float)--bomba atomica para salvar o mundo
+ , wjonas::(Float, Float)--jonas vitorioso
+ , djonas::(Float, Float)--jonas morto
+ , dijonas::(Float, Float)--jonas morto por inseticida
  , upButton::Bool -- botao pressionado
  , downButton::Bool
  , rightButton::Bool
@@ -19,6 +25,14 @@ imgjonas = (Graphics.Gloss.Game.png "imgs/jonas.png")
 imgsbp = (Graphics.Gloss.Game.png "imgs/sbpt.png")
 imgmmoscas1 = (Graphics.Gloss.Game.png "imgs/matamoscas1t.png")
 imgmmoscas2 = (Graphics.Gloss.Game.png "imgs/matamoscas2t.png")
+imgraid = (Graphics.Gloss.Game.png "imgs/raidt.png")
+imgaranha = (Graphics.Gloss.Game.png "imgs/aranhat.png")
+imgbomba = (Graphics.Gloss.Game.png "imgs/bombaatomicat.png")
+imgwjonas = (Graphics.Gloss.Game.png "imgs/winnerjonast.png")
+imgdjonas = (Graphics.Gloss.Game.png "imgs/deadjonast.png")
+imgdijonas = (Graphics.Gloss.Game.png "imgs/deadjonasinseticidat.png")
+imginsetatk = (Graphics.Gloss.Game.png "imgs/matamoscas2t.png")
+imgspray = (Graphics.Gloss.Game.png "imgs/esporradat.png")
 
 main::IO()
 main = play
@@ -40,10 +54,16 @@ main = play
   initialModel :: World
   initialModel = Game 
    {jonas = (-490,-280)
-   , sbp = (0, 0)
+   , sbp = (-100, 100)
    , upButton = False
    , matamoscas = (100,100)
    , matamoscasstate = 0
+   , raid = (-80, 200)
+   , aranha = (-60, -200)
+   , bomba = (480, 250)
+   , wjonas = (1000, 1000)
+   , djonas = (1000, 1000)
+   , dijonas = (1000,1000)
    , downButton = False
    , rightButton = False
    , leftButton = False
@@ -58,27 +78,24 @@ main = play
 
   drawingFunc :: World -> Picture
   drawingFunc w = pictures 
-   [translate (leftArg (sbp w)) (rightArg (sbp w)) imgsbp
-   , translate (leftArg (jonas w)) (rightArg (jonas w)) imgjonas
-   , translate ((leftArg (matamoscas w))+1000.0*(matamoscasstate w)) ((rightArg(matamoscas w))+1000.0*(matamoscasstate w)) imgmmoscas1
-   , translate ((leftArg (matamoscas w))+1000.0-(1000.0*(matamoscasstate w))) ((rightArg(matamoscas w))+1000.0-(1000.0*(matamoscasstate w))) imgmmoscas2
-   , pictures (let (x,y,ox,oy) = ((map leftArg (map rightArg (map getRekt (paredes w))))
+   [pictures (let (x,y,ox,oy) = ((map leftArg (map rightArg (map getRekt (paredes w))))
    	                              , (map rightArg (map rightArg (map getRekt (paredes w))))
    	                              , (map leftArg (map leftArg (map getRekt (paredes w))))
    	                              , (map rightArg (map leftArg (map getRekt (paredes w))))) 
                 in map myTranslate (zipao (map calculoZipao (zip2 ox x)) (map calculoZipao (zip2 oy y)) (map myRecSolid (zip2 x y))))
+   , translate (leftArg (sbp w)) (rightArg (sbp w)) imgsbp
+   , translate (leftArg (raid w)) (rightArg (raid w)) imgraid
+   , translate ((leftArg (matamoscas w))+1000.0*(matamoscasstate w)) ((rightArg(matamoscas w))+1000.0*(matamoscasstate w)) imgmmoscas1
+   , translate ((leftArg (matamoscas w))+1000.0-(1000.0*(matamoscasstate w))) ((rightArg(matamoscas w))+1000.0-(1000.0*(matamoscasstate w))) imgmmoscas2
+   , translate (leftArg (aranha w)) (rightArg (aranha w)) imgaranha
+   , translate (leftArg (bomba w)) (rightArg (bomba w)) imgbomba
+   , translate (leftArg (jonas w)) (rightArg (jonas w)) imgjonas
+   , translate ((leftArg (sbp w))-25) ((rightArg (sbp w))+10) imgspray
+   , translate ((leftArg (raid w))-25) ((rightArg (raid w))+10) imgspray
+   , translate (leftArg (wjonas w)) (rightArg (wjonas w)) imgwjonas
+   , translate (leftArg (djonas w)) (rightArg (djonas w)) imgdjonas
+   , translate (leftArg (dijonas w)) (rightArg (dijonas w)) imgdijonas
    ]
-
---leftArg(leftArg x) rightArg(leftArg x)
---map translate (map leftArg (map rightArg x)) (map rightArg (map rightArg x))
-  
---  map myTranslate (zipao 
-  --(map leftArg (map leftArg (map getRekt paredes)))--x vetor
-  --(map rightArg (map leftArg (map getRekt paredes)))--y vetor
-  --()--map dos retangulos
-  --)
-
-  
 
   inputHandler :: Event -> World -> World
   inputHandler (EventKey (SpecialKey KeyUp) Down _ _) w = w{upButton = True}
@@ -97,10 +114,12 @@ main = play
   inputHandler (EventKey (Char 'd') Up _ _) w = w{rightButton = False}
   inputHandler (EventKey (Char 'a') Down _ _) w = w{leftButton = True}
   inputHandler (EventKey (Char 'a') Up _ _) w = w{leftButton = False}
+  --inputHandler (EventKey (Char 'r') Down _ _) w = (initialModel w)
   inputHandler _ w = w
 
   updateFunc :: Float -> World -> World
-  updateFunc _ w = if (colisao (jonas w) (sbp w))||(colisao (jonas w) (matamoscas w)) then w{jonas = (1000,1000), sbp = (1000,1000)}
+  updateFunc _ w = if (colisao (jonas w) (aranha w))||(colisao (jonas w) (matamoscas w))||(colisao (jonas w) (raid w))||(colisao (jonas w) (sbp w)) then w{jonas = (1000,1000), sbp = (1000,1000), djonas = (0,0)}
+   else if (colisaoi (jonas w) (sbp w))||(colisaoi (jonas w) (raid w)) then w{jonas = (1000,1000), sbp = (1040, 1000), dijonas = (0,0)}
    else (changeMMState.movementCompute.getWorldFrame) w
 
 getRekt::([a],[a]) -> ((a,a),(a,a))
@@ -148,6 +167,12 @@ colisao::(Float,Float)->(Float,Float)->Bool
 colisao (x1,y1) (x2,y2) = 
  if (x1<=x2 && (x1+10)>=x2 && y1<=y2 && (y1+10)>=y2)||(x1<=x2 && (x1+10)>=x2 && y1>=y2 && (y1-10)<=y2)|| ---baixo esquerda, cima esquerda
  (x1>=x2 && (x1-10)<=x2 && y1<=y2 && (y1+10)>=y2)||(x1>=x2 && (x1-10)<=x2 && y1>=y2 && (y1-10)<=y2) ---baixo direita, cima direita
+ then True else False
+
+colisaoi::(Float,Float)->(Float,Float)->Bool
+colisaoi (x1,y1) (x2,y2) = 
+ if (x1<=(x2-40) && (x1+10)>=(x2-40) && y1<=(y2+10) && (y1+10)>=(y2+10))||(x1<=(x2-40) && (x1+10)>=(x2-40) && y1>=(y2+10) && (y1-10)<=(y2+10))|| ---baixo esquerda, cima esquerda
+ (x1>=(x2-40) && (x1-10)<=(x2-10) && y1<=(y2+10) && (y1+10)>=(y2+10))||(x1>=(x2-40) && (x1-10)<=(x2-10) && y1>=(y2+10) && (y1-10)<=(y2+10)) ---baixo direita, cima direita
  then True else False
 
 colisaoParedes::World -> Int -> (Float, Float)
