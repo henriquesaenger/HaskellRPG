989f9d4752ca8f27c45b60874962d581d3063058
Gustavo Barbosa <gbdsantos@inf.ufpel.edu.br>
2019-07-09 16:49:54 -0300

    retiradas classes inuteis

--- main.hs
+++
@@ -1,7 +0,0 @@
-import person.hs
-
-data Point= Point{x::Int, y::Int}		--cria um tipo ponto para setar a posição de um personagem no mapa
- deriving(Show)
-
-data Map= [Point]						--cria o tipo mapa para poder montar os mapas referentes ao jogo
- deriving(Show)
